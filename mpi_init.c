8fbef5ad6c99077f92c0d8879123784d23c7b08c
Parakkrama24 <parakkramadasanayaka.2@gmail.com>
2026-03-30 16:15:34 +0000

    Initial commit

---
+++ mpi_init.c
@@ -0,0 +1,36 @@
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+  int rank, size;
+  int number;
+
+  //Initilize 
+  MPI_Init(&argc,&argv);
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+  if(rank==0){
+
+    number= 100;
+    printf("Process %d sending number %d to process 1\n",rank,number);
+    MPI_Send(&number,1,MPI_INT,1,0,MPI_COMM_WORLD);
+
+    
+  }
+
+  else if (rank==1){
+
+    MPI_Recv(&number,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+    printf("Process %d received number %d from process 0\n",rank,number);
+  }
+
+  MPI_Finalize();
+  return 0;
+
+
+}
+
